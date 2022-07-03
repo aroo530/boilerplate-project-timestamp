1ffe3657f99d8f5d9e676cea6cde527160344a29
Arsany Milad <66312772+aroo530@users.noreply.github.com>
2022-07-03 17:37:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,21 +29,23 @@ app.get("/api", (_req, res) => {
     utc: tempDate.toUTCString(),
   });
 });
-app.get("/api/:input", (_req, res) => {
-  const input = _req.params.input;
-  // I could create a date object now but I wanted to make the code a bit more symmetrical
-  let tempDate;
-  if (new Date(input) != "Invalid Date") {
-    tempDate = new Date(input);
+
+app.get("/api/:date", (req, res) => {
+  const date = req.params.date;
+  if (Number.isInteger(Number(date))) {
+    tempDate = new Date(parseInt(date));
+    console.log("date is unix");
     res.json({
-      unix: new Date(tempDate).getTime(),
-      utc: new Date(tempDate).toUTCString(),
+      unix: new Date(parseInt(date)).getTime(),
+      utc: new Date(parseInt(date)).toUTCString(),
     });
-  } else if (new Date(parseInt(input) != "Invalid Date")) {
-    tempDate = new Date(parseInt(input));
+    //compare input to regex
+  } else if (/\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/.test(date)) {
+    tempDate = new Date(date);
+    console.log("input is utc");
     res.json({
-      unix: new Date(parseInt(input)).getTime(),
-      utc: new Date(parseInt(input)).toUTCString(),
+      unix: tempDate.getTime(),
+      utc: tempDate.toUTCString(),
     });
   } else {
     res.json({
@@ -52,7 +54,26 @@ app.get("/api/:input", (_req, res) => {
   }
 });
 
+// app.get("/api/:input", (_req, res) => {
+//   const input = _req.params.input;
+//   // I could create a date object now but I wanted to make the code a bit more symmetrical
+//   let tempDate;
+//   if (new Date(input) != "Invalid Date") {
+//     console.log("first if");
+//     tempDate = new Date(input);
+//     res.json({
+//       unix: new Date(tempDate).getTime(),
+//       utc: new Date(tempDate).toUTCString(),
+//     });
+//   } else {
+//     res.json({
+//       error: "Invalid Date",
+//     });
+//   }
+// });
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+//process.env.PORT
+var listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
